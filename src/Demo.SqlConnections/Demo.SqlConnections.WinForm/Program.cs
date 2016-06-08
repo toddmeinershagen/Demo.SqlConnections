3eb4c64fcfd907c8c5a4f5c5368959ff6edcf5bf
Todd Meinershagen <todd@meinershagen.net>
2016-06-08 18:23:12 -0500

    Adding the .ico file to the project.

--- src/Demo.SqlConnections/Demo.SqlConnections.WinForm/Program.cs
+++ src/Demo.SqlConnections/Demo.SqlConnections.WinForm/Program.cs
@@ -13,7 +13,7 @@ namespace Demo.SqlConnections.WinForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
